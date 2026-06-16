3675922f0dd133d3df6cd7f57cd15c8961c530a9
rinaudo-julian <rinaudojulian90@gmail.com>
2026-06-16 18:13:12 -0300

    refactor

--- backend/src/main/java/com/utn/backend/model/Categoria.java
+++ backend/src/main/java/com/utn/backend/model/Categoria.java
@@ -1,21 +1,18 @@
 package com.utn.backend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
-@Entity(name = "categorias")
+@Entity
+@Table(name = "categorias")
 public class Categoria extends Base {
     @Column(nullable = false, unique = true)
     private String nombre;
     private String descripcion;
-    @OneToMany(mappedBy = "categoria")
+    @Transient
     private Set<Producto> productos = new HashSet<>();
 }
